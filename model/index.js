a871df658503b91d5d361924cfc0498841fb909b
Ifeanyichukwuwebdeve <kingifean@gmail.com>
2020-10-23 15:41:05 +0100

    Connected the files

--- model/index.js
+++ model/index.js
@@ -1,23 +1,27 @@
 const mongoose = require('mongoose')
 
 if (process.env.NODE_ENV === 'production') {
-  mongoose.connect(`mongodb://mongo/curriculumapp`, {
+  mongoose.connect(`mongodb://mongo/jolet-dev`, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
   })
 } else {
-  mongoose.connect('mongodb://localhost:27017/curriculumapp', {
+  mongoose.connect('mongodb://localhost/jolet-dev', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
   })
 }
 
-const Curriculum = require('./Curriculum')
+const GameStatus = require('./GameStatus')
 const User = require('./User')
 const Verification = require('./Verification')
 
 module.exports = {
-  Curriculum,
+  GameStatus,
   User,
   Verification
 }
